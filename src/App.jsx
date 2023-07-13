3fc2b4a42dfc54a89aee51d8c7dd191a47b245a4
Felipe Fernandez <felipefelizx@gmail.com>
2023-07-13 01:22:27 -0400

    Style - diseño para Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import "./style/index.scss";
 import Login from "./components/LoginPage";
 
 function App() {
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,3 +1,5 @@
+import spotifyLogo from "../assets/spotify-logo.svg";
+
 const Login = () => {
   const handleLogin = () => {
     const clientID = "77f10df7f21e404faf5991c4b9e4be2c"; 
@@ -9,8 +11,16 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleLogin}>Iniciar sesión con Spotify</button>
+    <div className="login__contenedor">
+      <img
+        src={spotifyLogo}
+        alt="Logo de Spotify"
+        className="login__logo-spotify"
+      />
+      <h1 className="login__texto-spotify-review">Spotify Review</h1>
+      <button className="login__boton-inicio-spotify" onClick={handleLogin}>
+        Iniciar sesión con Spotify
+      </button>
     </div>
   );
 };
