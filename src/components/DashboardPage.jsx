8d7c4e20185135b944a01e6287d4920659a6dfba
Felipe Fernandez <felipefelizx@gmail.com>
2023-07-14 19:47:01 -0400

    Fix - icono + texto inservible

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -42,7 +42,6 @@ const Dashboard = () => {
 
   return (
     <>
-    <h1>DASHBOARD</h1>
       {/* Si hay token, se muestran 6 opciones*/}
       {userData && (
         <div className="dashboard-container">
